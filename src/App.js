5fb08395d74a8af2e43863a1730f85f2156fd698
abrarrakin <63086922+abrar-rakin@users.noreply.github.com>
2022-03-27 01:15:02 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [randomDoggo, setRandomDoggo] = useState("");
+  const fetchDoggo = async () => {
+    const dogUrl = "https://dog.ceo/api/breeds/image/random";
+    try {
+      const response = await fetch(dogUrl);
+      const json = await response.json();
+      setRandomDoggo(json.message);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchDoggo();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainDiv">
+      <h1>Get a random doggo!</h1>
+      <img src={randomDoggo} alt="random dog" id="dogImg"/>
+      <button onClick={fetchDoggo}>Get Another Doggo</button>
     </div>
   );
 }
